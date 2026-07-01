60f7dcc10b2002db456bcaa44a60b9881e582d4c
vioviovio <3020911615@qq.com>
2026-07-01 22:04:34 +0800

    Initial commit: VIO AI Agent project

---
+++ src/main/java/com/vio/vioaiagent/VioAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.vio.vioaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VioAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VioAiAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vio/vioaiagent/VioAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vio.vioaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VioAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
